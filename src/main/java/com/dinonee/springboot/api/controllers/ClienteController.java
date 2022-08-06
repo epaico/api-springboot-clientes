4b0b71d9f4f776a840545ae964e8ec0baf234f26
Elvis Paico <epaico.c@gmail.com>
2022-08-05 22:25:05 -0500

    Update Cors

--- src/main/java/com/dinonee/springboot/api/controllers/ClienteController.java
+++ src/main/java/com/dinonee/springboot/api/controllers/ClienteController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
